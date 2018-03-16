2b59532adb4bae8301df754ed4cf893f07557e5e
goalkeeer <goalkeeer@gmail.com>
2018-03-16 15:45:43 +0500

    delete webexample

--- _project_/urls.py
+++ _project_/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^webexaple/', include('webexample.urls')),
     url(r'^', include('main.urls')),
     url(r'^news', include('news.urls')),
 ]
